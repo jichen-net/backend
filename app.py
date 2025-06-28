e6d9f0b2ab1b8edc21ef41f8e8d6406846e68a61
jichen-net <150653157+jichen-net@users.noreply.github.com>
2025-06-29 02:22:52 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 
-from flask import Flask, render_template
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    return jsonify({"message": "Flask app is running on Railway using Gunicorn!"})
